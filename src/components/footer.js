73011dd16e81cd2262e4217569b7a2a11e6b9e03
Jackie Tan <jackilyn.tan@gmail.com>
2023-06-20 19:17:15 -0400

    jtstart

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="https://github.com/username">GitHub</a></li>
+        <li><a href="https://linkedin.com/in/username">LinkedIn</a></li>
+        <li><a href="https://third-platform.com/username">Third Platform</a></li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
